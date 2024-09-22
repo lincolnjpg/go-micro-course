c02f24c0e9e9fdab99453088e2b97f7f42fb50a3
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-09-22 20:10:51 -0300

    add repository pattern to auth service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -19,8 +19,7 @@ const webPort = "8002"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	Repo data.Repository
 }
 
 func main() {
@@ -31,10 +30,7 @@ func main() {
 		log.Panic("Can't connect to PostgreSQL")
 	}
 
-	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
-	}
+	app := Config{}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -84,4 +80,9 @@ func connectToDB() *sql.DB {
 		log.Println("Backing off for 2 seconds...")
 		time.Sleep(2 * time.Second)
 	}
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }
